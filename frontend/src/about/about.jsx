85fe78f82f1cf31605ff8bcb53f202924e6dee70
morganacruz <morganacruzalmeida@gmail.com>
2019-12-20 20:43:53 -0300

    criaçao do frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa historia</h2>
+        <p>Lorem Ipsum.....</p>
+        <h2>Missao e visao</h2>
+        <p>Lorem ipsum</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum...</p>
+    </div>
+)
